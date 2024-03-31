29a7938693b8396f4a906058735141de499dcfff
Saurabhgawade <72007247+Saurabhgawade@users.noreply.github.com>
2024-03-31 15:59:33 +0530

    Add files via upload

---
+++ EntityToDatabase/Data/EntityToDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EntityToDatabase.Models;
+
+namespace EntityToDatabase.Data
+{
+    public class EntityToDatabaseContext : DbContext
+    {
+        public EntityToDatabaseContext (DbContextOptions<EntityToDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EntityToDatabase.Models.Student> Student { get; set; } = default!;
+    }
+}
